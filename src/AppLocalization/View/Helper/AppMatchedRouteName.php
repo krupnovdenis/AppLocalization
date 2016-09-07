0181b371a743e0a3526cda85310939c05575edfc
Dima Halytskyi <galiskyu@gmail.com>
2016-09-07 10:43:32 +0300

    View helper rewrited

--- src/AppLocalization/View/Helper/AppMatchedRouteName.php
+++ src/AppLocalization/View/Helper/AppMatchedRouteName.php
@@ -6,14 +6,26 @@ use Zend\View\Helper\AbstractHelper;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\View\HelperPluginManager;
 
 class AppMatchedRouteName extends AbstractHelper implements ServiceLocatorAwareInterface {
 
     private $serviceLocator;
 
-    public function __invoke() {
+    public function __invoke($newParams = array()) {
+        /** @var \Zend\Mvc\Router\RouteMatch $routeMatch */
         $routeMatch = $this->serviceLocator->getServiceLocator()->get('Application')->getMvcEvent()->getRouteMatch();
-        return $routeMatch->getMatchedRouteName();
+        $oldParams = $routeMatch->getParams();
+        $params = array_merge($oldParams, $newParams);
+
+        /** @var \Zend\View\HelperPluginManager $viewHelperController */
+        $viewHelperController = $this->getServiceLocator()->getServiceLocator()->get('ViewHelperManager');
+
+        /** @var \Zend\View\Helper\Url $urlHelper */
+        $urlHelper = $viewHelperController->get('url');
+
+        $trueUrl = $urlHelper(null, $params);
+        return $trueUrl;
     }
 
     /**
